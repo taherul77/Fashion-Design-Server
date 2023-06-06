f5357b3f2de0208526f7925fa9607e9f6b617cf0
taherul77 <taherul2317@gmail.com>
2023-06-07 01:52:05 +0600

    get course data

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const express= require('express');
+const app =express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const port = 3000
 
+const port = process.env.PORT || 5000;
 
+//middleware
 
+app.use(cors());
+app.use(express.json());
 
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.iroozgm.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iroozgm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,17 +30,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+const courseCollection = client.db('summerCamp').collection('course');
+
+
+
+
+app.get('/course', async(req,res)=>{
+    const data = courseCollection.find();
+    const result = await data.toArray();
+    res.send(result);
+  
+  })
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 
 
