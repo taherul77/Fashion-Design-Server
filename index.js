fce56a764a74131e4892d617eeb8c3edcf81309b
taherul77 <taherul2317@gmail.com>
2023-06-11 07:48:45 +0600

    update payment api

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express= require('express');
-const app =express();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const SSLCommerzPayment = require("sslcommerz-lts");
 
+require("dotenv").config();
+
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false; //true for live, false for sandbox
 
 const port = process.env.PORT || 5000;
 
@@ -12,28 +17,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const verifyJWT = (req,res,next) =>{
+const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  
-  if(!authorization){
-    return res.status(401).send({error: true , message: 'unauthorized access'});
-    
-  } 
-  const token = authorization.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN,(err,decoded)=>{
-      if(err){
-        return res.status(401).send({error: true, message: 'unauthorized access'})
-      }
-      req.decoded =decoded;
-      next();
-    })
-  
+
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iroozgm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,7 +46,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -50,162 +54,219 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const courseCollection = client.db("summerCamp").collection("course");
 
-const courseCollection = client.db('summerCamp').collection('course');
+    const cartCollection = client.db("summerCamp").collection("cart");
+    const userCollection = client.db("summerCamp").collection("user");
+    const orderCollection = client.db("summerCamp").collection("order");
 
-const cartCollection = client.db("summerCamp").collection("cart");
-const userCollection = client.db("summerCamp").collection("user");
+    app.post("/jwt", (req, res) => {
+      user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "5h",
+      });
+      res.send({ token });
+    });
 
-app.post('/jwt',(req,res)=>{
-  user = req.body;
-  const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn: '5h'});
-  res.send({token});
-})
-
-
-app.post('/users',async(req,res)=>{
-  const user = req.body;
-  const email = user.email;
-  const query = {email};
-  const existingUser = await userCollection.findOne(query);
-  if (existingUser) {
-   return res.send( {message: ''});
-  }
-  
- 
-  const result = await userCollection.insertOne(user);
-  res.send(result);
-
-
-})
-
-app.get('/users',  async(req,res)=>{
-  const result = await userCollection.find().toArray();
-  res.send(result);
-  
-  })
-
-
-  app.patch('/users/admin/:id',async(req,res)=>{
-    const id= req.params.id;
-    const filter = {_id: new ObjectId(id)}
-    const updateDoc ={
-      $set: {
-        role: 'admin' 
-      },
-      
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
     };
-    const result = await userCollection.updateOne(filter,updateDoc);
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const email = user.email;
+      const query = { email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "" });
+      }
+
+      const result = await userCollection.insertOne(user);
       res.send(result);
-  })
-  app.patch('/users/instructor/:id', async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) };
-    const updateDoc = {
-      $set: {
-        role: 'instructor'
-      },
-    };
-    const result = await userCollection.updateOne(filter, updateDoc);
-    res.send(result);
-  });
-  
+    });
 
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const { price, title, customerName, customerEmail } = order;
+      const tran_id = new ObjectId().toString();
+      const data = {
+        total_amount: price,
+        currency: "BDT",
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: "http://localhost:3030/fail",
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: title,
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: customerName,
+        cus_email: customerEmail,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
 
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
+        const finalOrder = {
+          order,
+          paidStatus: false,
+          transactionId: tran_id,
+        };
 
+        const result = orderCollection.insertOne(finalOrder);
+      });
 
+      app.post("/payment/success/:tran_id", async (req, res) => {
+        console.log(req.params);
+        const { tran_id } = req.params;
 
+        const result = await orderCollection.updateOne(
+          {
+            transactionId: tran_id,
+          },
+          {
+            $set: {
+              paidStatus: true,
+            },
+          }
+        );
 
+        if(result.modifiedCount>0){
+          res.redirect(`http://localhost:5173/payment/success/${tran_id}`)
+        }
+      });
+    });
 
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.patch("/users/admin/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
 
-
-  app.get('/users/admin/:email',async(req,res)=>{
-
-    const email= req.params.email;
-    
-    const query = {email:email}
-    const user = await userCollection.findOne(query);
-    const result = { admin : user?.role === 'admin'}
-    res.send(result);
-  })
-  app.delete('/users/delete/:id', async (req,res)=>{
-    const id =req.params.id;
-    console.log(id);
-    const query = {_id: new ObjectId(id)};
-    const result = await userCollection.deleteOne(query);
-    res.send(result)
-    
-    })
-
-
-app.get('/course', async(req,res)=>{
-    const data = courseCollection.find();
-    const result = await data.toArray();
-    res.send(result);
-  
-  })
-
-  app.get('/courses',  async(req,res)=>{
-    const {email} = req.query;
-    console.log(email);
-
- 
-  
-    const query = {'instructor.email' : email};
-    const result = await courseCollection.find(query).toArray();
-
-    res.send(result)
-   
-
-  })
-
-  app.post('/course-cart', async(req,res)=>{
-
-    const item = req.body;
-    console.log(item);
-    const result = await cartCollection.insertOne(item);
-    res.send(result);
-  })
-  app.get('/course-cart', verifyJWT, async(req, res)=>{
-
-    const email = req.query.email;
-   
-    if(!email){
-      res.send([]);
-    }
-   
-    const decodedEmail = req.decoded.email;
-    if(email !== decodedEmail){
-      return req.status(403).send({error: true, message: 'forbidden access'})
-    }
-    
-    const query = { email : email};
-    const result = await cartCollection.find(query).toArray();
-    res.send(result);
-    
-    })
-    app.delete('/course/delete/:id', async (req,res)=>{
-      const id =req.params.id;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+    app.delete("/users/delete/:id", async (req, res) => {
+      const id = req.params.id;
       console.log(id);
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/course", async (req, res) => {
+      const data = courseCollection.find();
+      const result = await data.toArray();
+      res.send(result);
+    });
+
+    app.get("/courses", async (req, res) => {
+      const { email } = req.query;
+      console.log(email);
+
+      const query = { "instructor.email": email };
+      const result = await courseCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    app.delete("/course/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await courseCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.post("/course-cart", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+    app.get("/course-cart", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return req
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/course/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
-      res.send(result)
-      
-      })
-
-
-
-
-
-
-
-
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -213,14 +274,10 @@ app.get('/course', async(req,res)=>{
 }
 run().catch(console.dir);
 
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
